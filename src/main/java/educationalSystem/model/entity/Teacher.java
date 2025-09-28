c27fcd2b3fb6e2d2e22d88895b295b0cea7cd722
Zahra <moradi.nehad.137@gmail.com>
2025-09-28 11:35:17 +0330

    add

--- src/main/java/educationalSystem/model/entity/Teacher.java
+++ src/main/java/educationalSystem/model/entity/Teacher.java
@@ -1,4 +1,31 @@
 package educationalSystem.model.entity;
 
+import com.google.gson.Gson;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class Teacher {
-}
+
+        private String name;
+        private String brand;
+        private LocalDate manDate;
+        private String color;
+        private String plate;
+
+
+        @Override
+        public String toString() {
+            Gson gson = new Gson();
+            return gson.toJson(this);
+        }
+    }
+
