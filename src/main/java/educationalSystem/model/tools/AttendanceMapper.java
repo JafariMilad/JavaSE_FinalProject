84545ef52cb5f7328e910d410e7cd1f68418b9be
MohadeseSaeediFard <mohadesesaeedifard@gmail.com>
2025-10-21 00:14:41 +0330

    db Updated

--- src/main/java/educationalSystem/model/tools/AttendanceMapper.java
+++ src/main/java/educationalSystem/model/tools/AttendanceMapper.java
@@ -14,7 +14,7 @@ public class AttendanceMapper {
                 .student(StudentService.getService().findById(resultSet.getInt("student_code")))
                 .attendanceStatus(AttendanceStatus.valueOf(resultSet.getString("attendance_status")))
                 .celass(CelassService.getService().findById(resultSet.getInt("celass_code")))
-                .session(SessionService.getService().findById(resultSet.getInt("session_Code")))
+                .session(SessionService.getService().findById(resultSet.getInt("session_code")))
                 .attendanceDate(resultSet.getDate("attendance_date").toLocalDate())
                 .lesson(LessonService.getService().findById(resultSet.getInt("lesson_code")))
                 .build();
--- src/main/resources/veiw/database_creator.sql
+++ src/main/resources/veiw/database_creator.sql
@@ -1,166 +1,175 @@
-
-
+CREATE TABLE users
+(
+    id          NUMBER         PRIMARY KEY,
+    first_name  NVARCHAR2(50)  NOT NULL,
+    last_name   NVARCHAR2(50)  NOT NULL,
+    email       NVARCHAR2(100) UNIQUE NOT NULL,
+    address     NVARCHAR2(100) NOT NULL,
+    national_Id NVARCHAR2(10)  UNIQUE,
+    birth_date  DATE           NOT NULL
+);
 CREATE SEQUENCE user_seq START WITH 1 INCREMENT BY 1;
-CREATE TABLE Users
-(
-    id NUMBER PRIMARY KEY,
-    username NVARCHAR2(50) UNIQUE NOT NULL,
-    password NVARCHAR2(100) NOT NULL,
-    firstname NVARCHAR2(50) NOT NULL,
-    lastname NVARCHAR2(50) NOT NULL,
-    email NVARCHAR2(100) UNIQUE NOT NULL,
-    address NVARCHAR2(100),
-    nationalId NVARCHAR2(10) UNIQUE,
-    birthdate DATE
-);
 
-CREATE SEQUENCE teacher_seq START WITH 1 INCREMENT BY 1;
-CREATE TABLE Teachers
+
+CREATE TABLE teachers
 (
-    id         number primary key,
     teacher_code NUMBER PRIMARY KEY,
-    user_id NUMBER UNIQUE NOT NULL,
-    lesson_code NUMBER UNIQUE NOT NULL,
-    CONSTRAINT fk_teacher_user FOREIGN KEY (user_id) REFERENCES USERS (id),
-        CONSTRAINT lesson_code FOREIGN KEY (lesson_code) REFERENCES COURSES (lesson_code)
+    lesson_code  NUMBER NOT NULL,
+    CONSTRAINT fk_teachers_lesson FOREIGN KEY (lesson_code) REFERENCES lessons (lesson_code)
 );
+CREATE SEQUENCE teacher_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE student_seq START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE STUDENTS
 (
-    student_code NUMBER PRIMARY KEY,
-    user_id NUMBER UNIQUE NOT NULL,
-    CONSTRAINT fk_student_user FOREIGN KEY (user_id) REFERENCES USERS (id)
+    student_code    NUMBER PRIMARY KEY,
+    enrollment_code NUMBER NOT NULL,
+    project_code    NUMBER NOT NULL,
+    exersise_code   NUMBER NOT NULL,
+    attendance_code NUMBER NOT NULL,
+    lesson_code     NUMBER NOT NULL,
+    CONSTRAINT fk_student_enrollment FOREIGN KEY (enrollment_code) REFERENCES ENROLLMENT (enrollment_code),
+    CONSTRAINT fk_student_project FOREIGN KEY (project_code) REFERENCES PROJECT (project_code),
+    CONSTRAINT fk_student_exersise FOREIGN KEY (exersise_code) REFERENCES exersise (exersise_code),
+    CONSTRAINT fk_student_attendance FOREIGN KEY (attendance_code) REFERENCES ATTENDANCE (attendance_code),
+    CONSTRAINT fk_student_lesson FOREIGN KEY (lesson_code) REFERENCES PROJECT (lesson_code)
 );
+CREATE SEQUENCE student_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE course_seq START WITH 1 INCREMENT BY 1;
-CREATE TABLE COURSES
+
+CREATE TABLE sessions
 (
-    class_code NUMBER PRIMARY KEY,
-    class_name NVARCHAR2(50) NOT NULL,
-    class_points NUMBER,
-    class_status NVARCHAR2(20),
-    teacher_code NUMBER NOT NULL,
-    CONSTRAINT fk_course_teacher FOREIGN KEY (teacher_code) REFERENCES TEACHERS (teacher_code)
+    session_code    NUMBER PRIMARY KEY,
+    celass_code     NUMBER NOT NULL,
+    lesson_code     NUMBER NOT NULL,
+    attendance_code NUMBER NOT NULL,
+    exersise_code   NUMBER NOT NULL,
+    CONSTRAINT fk_sessions_celass FOREIGN KEY (celass_code) REFERENCES celass (celass_code),
+    CONSTRAINT fk_sessions_lesson FOREIGN KEY (lesson_code) REFERENCES lessons (lesson_code),
+    CONSTRAINT fk_sessions_attendance FOREIGN KEY (attendance_code) REFERENCES ATTENDANCE (attendance_code),
+    CONSTRAINT fk_sessions_exersise FOREIGN KEY (exersise_code) REFERENCES exersise (exersise_code)
 );
-
-CREATE SEQUENCE lesson_seq START WITH 1 INCREMENT BY 1;
-CREATE TABLE Lessons
-(
-    lesson_code NUMBER PRIMARY KEY,
-    class_code NUMBER NOT NULL,
-    lesson_name NVARCHAR2(50) NOT NULL,
-    lesson_status NVARCHAR2(20),
-    lesson_date DATE NOT NULL,
-    lesson_teacher NUMBER,
-    CONSTRAINT fk_lesson_course FOREIGN KEY (class_code) REFERENCES COURSES (class_code),
-    CONSTRAINT fk_lesson_teacher FOREIGN KEY (lesson_teacher) REFERENCES TEACHERS (teacher_code)
-);
-
 CREATE SEQUENCE session_seq START WITH 1 INCREMENT BY 1;
-CREATE TABLE SESSION
-(
-    session_id NUMBER PRIMARY KEY,
-    class_code NUMBER NOT NULL,
-    lesson_code NUMBER NOT NULL,
-    session_status NVARCHAR2(20),
-    session_date DATE,
-    teacher_attendance NVARCHAR2(20),
-    CONSTRAINT fk_session_course FOREIGN KEY (class_code) REFERENCES COURSES (class_code),
-    CONSTRAINT fk_session_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code)
-);
 
-CREATE SEQUENCE enrollment_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE lessons
+(
+    lesson_code     NUMBER          PRIMARY KEY,
+    lesson_name     NVARCHAR2(50)   NOT NULL,
+    lesson_status   NVARCHAR2(20)   NOT NULL,
+    start_date      DATE            NOT NULL,
+    end_date        DATE            NOT NULL,
+    lesson_price    NUMBER          NOT NULL,
+    lesson_time     NVARCHAR2(50)   NOT NULL,
+    lesson_teacher  NUMBER          NOT NULL,
+    student_code    NUMBER          NOT NULL,
+    celass_code     NUMBER          NOT NULL,
+    CONSTRAINT fk_lesson_teacher FOREIGN KEY (lesson_teacher) REFERENCES TEACHERS (teacher_code),
+    CONSTRAINT fk_lesson_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
+    CONSTRAINT fk_lesson_celass FOREIGN KEY (celass_code) REFERENCES COURSES (celass_code)
+CREATE SEQUENCE lesson_seq START WITH 1 INCREMENT BY 1;
+
+
 CREATE TABLE ENROLLMENT
 (
-    enrollment_code NUMBER PRIMARY KEY,
-    student_code NUMBER NOT NULL,
-    class_code NUMBER NOT NULL,
-    enrollment_status NVARCHAR2(20),
-    register_date DATE,
-    lesson_code NUMBER,
-    CONSTRAINT fk_enroll_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
-    CONSTRAINT fk_enroll_course FOREIGN KEY (class_code) REFERENCES COURSES (class_code),
-    CONSTRAINT fk_enroll_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code)
+    enrollment_code   NUMBER          PRIMARY KEY,
+    enrollment_status NVARCHAR2(20)   NOT NULL,
+    lesson_code       NUMBER          NOT NULL,
+    teacher_code      NUMBER          NOT NULL,
+    student_code      NUMBER          NOT NULL,
+    payment_code      NUMBER          NOT NULL,
+    register_date     DATE            NOT NULL,
+    CONSTRAINT fk_enrollment_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code),
+    CONSTRAINT fk_enrollment_teacher FOREIGN KEY (teacher_code) REFERENCES teachers (teacher_code),
+    CONSTRAINT fk_enrollment_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
+    CONSTRAINT fk_enrollment_payment FOREIGN KEY (payment_code) REFERENCES Payments (payment_code)
 );
+CREATE SEQUENCE enrollment_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE payment_seq START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE Payments
 (
-    payment_id NUMBER PRIMARY KEY,
-    payment_type NVARCHAR2(20),
-    payment_status NVARCHAR2(20),
-    payment_date DATE,
-    enrollment_code NUMBER,
-    user_id NUMBER,
-    CONSTRAINT fk_payment_enroll FOREIGN KEY (enrollment_code) REFERENCES ENROLLMENT (enrollment_code),
-    CONSTRAINT fk_payment_user FOREIGN KEY (user_id) REFERENCES USERS (id)
+    payment_code NUMBER             PRIMARY KEY,
+    payment_status NVARCHAR2(20)    NOT NULL,
+    payment_date DATE               NOT NULL,
+    student_code NUMBER             NOT NULL,
+    lesson_code NUMBER              NOT NULL,
+    CONSTRAINT fk_payment_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
+    CONSTRAINT fk_payment_lesson FOREIGN KEY (lesson_code) REFERENCES lessons (lesson_code)
 );
+CREATE SEQUENCE payment_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE report_seq START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE Reports
 (
-    report_id NUMBER PRIMARY KEY,
-    report_date DATE,
-    student_code NUMBER NOT NULL,
-    teacher_code NUMBER NOT NULL,
-    class_code NUMBER NOT NULL,
+    report_code  NUMBER   PRIMARY KEY,
+    report_date  DATE     NOT NULL,
+    student_code NUMBER   NOT NULL,
+    lesson_code  NUMBER   NOT NULL,
     CONSTRAINT fk_report_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
-    CONSTRAINT fk_report_teacher FOREIGN KEY (teacher_code) REFERENCES TEACHERS (teacher_code),
-    CONSTRAINT fk_report_course FOREIGN KEY (class_code) REFERENCES COURSES (class_code)
+    CONSTRAINT fk_report_lesson FOREIGN KEY (lesson_code) REFERENCES lessons (lesson_code)
 );
+CREATE SEQUENCE report_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE project_seq START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE PROJECT
 (
-    project_code NUMBER PRIMARY KEY,
-    project_title nvarchar2(100),
-    student_code NUMBER NOT NULL,
-    lesson_code NUMBER NOT NULL,
-    session_code NUMBER NOT NULL,
-    status  nvarchar2(20),
+    project_code  NUMBER            PRIMARY KEY,
+    project_title nvarchar2(100)    NOT NULL,
+    student_code  NUMBER            NOT NULL,
+    lesson_code   NUMBER            NOT NULL,
+    session_code  NUMBER            NOT NULL,
+    status        nvarchar2(20)     NOT NULL,
     CONSTRAINT fk_project_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
     CONSTRAINT fk_project_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code),
     CONSTRAINT fk_project_session FOREIGN KEY (session_code) REFERENCES SESSION (session_code)
 );
+CREATE SEQUENCE project_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE Exersise_seq START WITH 1 INCREMENT BY 1;
-CREATE TABLE Exersise
+
+CREATE TABLE exersise
 (
     exersise_code NUMBER PRIMARY KEY,
-    exersise_date DATE,
-    student_code NUMBER NOT NULL,
-    lesson_code NUMBER NOT NULL,
-    session_code NUMBER NOT NULL,
-    CONSTRAINT fk_Exersise_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
+    exersise_date DATE NOT NULL,
+    lesson_code   NUMBER NOT NULL,
+    session_code  NUMBER NOT NULL,
+    student_code  NUMBER NOT NULL,
     CONSTRAINT fk_Exersise_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code),
-    CONSTRAINT fk_Exersise_session FOREIGN KEY (session_code) REFERENCES SESSION (session_code)
+    CONSTRAINT fk_Exersise_session FOREIGN KEY (session_code) REFERENCES SESSION (session_code),
+    CONSTRAINT fk_Exersise_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code)
 );
+CREATE SEQUENCE Exersise_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE grade_seq START WITH 1 INCREMENT BY 1;
-CREATE TABLE GRADES
+
+CREATE TABLE celass
 (
-    grade_id NUMBER PRIMARY KEY,
-    student_code NUMBER NOT NULL,
-    class_code NUMBER NOT NULL,
-    lesson_code NUMBER,
-    grade_value NUMBER(3,1),
-    CONSTRAINT fk_grade_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
-    CONSTRAINT fk_grade_course FOREIGN KEY (class_code) REFERENCES COURSES (class_code),
-    CONSTRAINT fk_grade_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code)
+    class_code      NUMBER PRIMARY KEY,
+    session_code    NUMBER NOT NULL,
+    lesson_code     NUMBER NOT NULL,
+    enrollment_code NUMBER NOT NULL,
+    student_code    NUMBER NOT NULL,
+    teacher_code    NUMBER NOT NULL,
+    CONSTRAINT fk_celass_session FOREIGN KEY (session_code) REFERENCES sessions (session_code),
+    CONSTRAINT fk_celass_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code),
+    CONSTRAINT fk_celass_enrollment FOREIGN KEY (enrollment_code) REFERENCES ENROLLMENT (enrollment_code),
+    CONSTRAINT fk_celass_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
+    CONSTRAINT fk_celass_teacher FOREIGN KEY (teacher_code) REFERENCES teachers (teacher_code)
 );
+CREATE SEQUENCE grade_seq START WITH 1 INCREMENT BY 1;
 
-CREATE SEQUENCE attendance_seq START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE ATTENDANCE
 (
-    attendance_id NUMBER PRIMARY KEY,
-    student_code NUMBER NOT NULL,
-    session_id NUMBER NOT NULL,
-    kelass_code  NUMBER NOT NULL,
-    status NVARCHAR2(20),
-    attendance_date date          not null,
-    CONSTRAINT fk_attend_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
-    CONSTRAINT fk_attend_session FOREIGN KEY (session_id) REFERENCES SESSIONS (session_id),
- CONSTRAINT fk_attend_kelass FOREIGN KEY (kelass_code) REFERENCES STUDENTS (kelass_code),
-
+    attendance_code     NUMBER          PRIMARY KEY,
+    student_code        NUMBER          NOT NULL,
+    attendance_status   NVARCHAR2(20)   NOT NULL,
+    celass_code         NUMBER          NOT NULL,
+    session_code        NUMBER          NOT NULL,
+    attendance_date     DATE            NOT NULL,
+    lesson_code         NUMBER          NOT NULL,
+    CONSTRAINT fk_attendance_student FOREIGN KEY (student_code) REFERENCES STUDENTS (student_code),
+    CONSTRAINT fk_attendance_celass FOREIGN KEY (celass_code) REFERENCES COURSES (celass_code),
+    CONSTRAINT fk_attendance_session FOREIGN KEY (session_code) REFERENCES sessions (session_code),
+    CONSTRAINT fk_attendance_lesson FOREIGN KEY (lesson_code) REFERENCES LESSONS (lesson_code)
 );
+CREATE SEQUENCE attendance_seq START WITH 1 INCREMENT BY 1;
